7a4d6eaa50a3a71b06e69a89f763d616209cae34
Bruno Raposo <brunoraposounb@gmail.com>
2025-10-08 17:41:24 -0300

    Feat: users crud controllers

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { connectDB } = require('./src/config/database');
 const collaboratorRoutes = require('./src/routes/collaboratorsRoutes.js'); 
 const userRoutes = require('./src/routes/userRoutes.js');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/register', userController.register);
+router.put('/:id', userController.update);
+router.get('/', userController.listUsers);
+router.get('/:id', userController.showUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
