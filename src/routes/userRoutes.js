cff2c87e0854a3d680fa74b3f62f92c0d31acf35
Bruno Raposo <brunoraposounb@gmail.com>
2025-10-10 16:53:56 -0300

    Feat: registration validation

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { authenticate, authorizeByRole, authorizeOwnResourceOrAdmin } = require('../middlewares/auth');
+const { validateUserRegistration } = require('../middlewares/validation')
 
-router.post('/register', userController.register);
+router.post('/register', validateUserRegistration, userController.register);
 router.put('/:id', authenticate, authorizeOwnResourceOrAdmin, userController.update);
 router.get('/', authenticate, authorizeByRole(['ADMIN']), userController.listUsers);
 router.get('/:id', authenticate, userController.showUser);
