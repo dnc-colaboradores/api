786842df7cd700ff28149ba1c9e3f0e0bfc0b0c0
Bruno Raposo <brunoraposounb@gmail.com>
2025-10-13 17:22:12 -0300

    Feat: add delete and put routes

--- src/routes/collaboratorsRoutes.js
+++ src/routes/collaboratorsRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getAllCollaborators, createCollaborator, getCollaboratorById } = require('../controllers/collaboratorsController');
+const { getAllCollaborators, createCollaborator, getCollaboratorById, updateCollaborator, deleteCollaboratoryById } = require('../controllers/collaboratorsController');
 const { authenticate, authorizeByRole } = require('../middlewares/auth');
 
 // Rotas CRUD 
 router.get('/', authenticate, getAllCollaborators); // Listar
 router.get('/:id', authenticate, getCollaboratorById) // Buscar por ID
 router.post('/', authenticate, authorizeByRole(['ADMIN']), createCollaborator); // Criar
+router.put('/:id', authenticate, authorizeByRole(['ADMIN']), updateCollaborator); // Atualizar por ID
+router.delete('/:id', authenticate, authorizeByRole(['ADMIN']), deleteCollaboratoryById); // Deletar
 
 module.exports = router;
